049c04854569e4fa371c3ce16c0375f99c9732fc
equation314 <equation618@gmail.com>
2024-07-15 15:43:21 +0000

    Deploying to gh-pages from @ arceos-org/x86_rtc@3e20ea7069fda643162075e4e44fa4493cde0d48 🚀

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["x86_rtc",{"t":"FNNNNNNNNNN","n":["Rtc","borrow","borrow_mut","from","get_unix_timestamp","into","new","set_unix_timestamp","try_from","try_into","type_id"],"q":[[0,"x86_rtc"],[11,"core::result"],[12,"core::any"]],"i":[0,3,3,3,3,3,3,3,3,3,3],"f":"`{{{b{c}}}{{b{e}}}{}{}}{{{b{dc}}}{{b{de}}}{}{}}{cc{}}{{{b{f}}}h}{ce{}{}}{{}f}{{{b{f}}h}j}{c{{l{e}}}{}{}}0{{{b{c}}}n{}}","D":"n","p":[[1,"reference"],[0,"mut"],[5,"Rtc",0],[1,"u64"],[1,"unit"],[6,"Result",11],[5,"TypeId",12]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAQAEAAAAAIAAwAJAAoACwA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["x86_rtc",["",[],["lib.rs"]]]\
+]'));
+createSrcSidebar();
